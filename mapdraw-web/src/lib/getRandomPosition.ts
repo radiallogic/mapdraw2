1036f1742869b460d6822bb80750b65c99fcd306
Max B <1492544+radiallogic@users.noreply.github.com>
2026-05-12 11:03:22 +0100

    Fixed issue with getRandomPosition loading window in leaflet

--- mapdraw-web/src/lib/getRandomPosition.ts
+++ mapdraw-web/src/lib/getRandomPosition.ts
@@ -1,14 +1,12 @@
-import { LatLng } from "leaflet";
+export type LatLng = {
+  lat: number;
+  lng: number;
+};
 
-export function getRandomPosition() {
+export function getRandomPosition(): LatLng {
   let n1 = Math.random();
   let n2 = Math.random();
   let pos = [n1 * (90 - -90) + -90, n2 * (80 - -180) + -180];
 
-  // ( ( n1 * (90 - -90)  + -90).toFixed(3) * 1 ) ,
-  // ( ( n2 * (80 - -180) + -180).toFixed(3) * 1 ) ,
-
-  const ll = new LatLng(pos[0], pos[1]);
-  console.log(" rand ll", ll);
-  return ll;
+  return { lat: pos[0], lng: pos[1] };
 }
